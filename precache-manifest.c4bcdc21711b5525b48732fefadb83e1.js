a2da7d04853ea038f199d20e98f04ce049864df1
Alessandro Macagno <ale.macagno@hotmail.it>
2020-01-20 12:05:10 +0100

    first commit

---
+++ precache-manifest.c4bcdc21711b5525b48732fefadb83e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abbd0e85ac1b0dc953a",
+    "url": "/test-vue-cli/css/app.1246a6d3.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/test-vue-cli/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9d9c95fe204c6a60203ad9f0d3008276",
+    "url": "/test-vue-cli/index.html"
+  },
+  {
+    "revision": "3abbd0e85ac1b0dc953a",
+    "url": "/test-vue-cli/js/app.4707e7d4.js"
+  },
+  {
+    "revision": "e064cb62a635469aa603",
+    "url": "/test-vue-cli/js/chunk-vendors.3a5e1e1d.js"
+  },
+  {
+    "revision": "38bd141cb205370c6a54cbc156e0f2b1",
+    "url": "/test-vue-cli/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-vue-cli/robots.txt"
+  }
+]);
